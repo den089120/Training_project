4eadd44d1d6a4d8cc7ffdb71aad9112307e2e67f
@den089120 <den089120@mail.ru>
2021-08-26 23:31:26 +0700

    Добавьте файлы проекта.

---
+++ Train_project/Models/ShoppingCart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Train_project.Models
+{
+    public class ShoppingCart
+    {
+        public int ProductId { get; set; }
+    }
+}
---
+++ Train_project/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $('.filter-button').click(function () {
+        
+        var value = $(this).attr('data-filter');
+        
+        if (value == 'all') {
+            $('.filter').show('1000');
+        } else {
+            $('.filter').not('.' + value).hide('3000');
+            $('.filter').filter('.' + value).show('3000');
+        }
+    })
+});
